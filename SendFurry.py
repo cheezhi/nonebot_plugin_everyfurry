0d779b1d6e89de1fbe766b75f2a463ca5a269fbf
ye土土 <53083230+ye-tutu@users.noreply.github.com>
2022-04-03 03:20:51 +0800

    Update SendFurry.py

--- SendFurry.py
+++ SendFurry.py
@@ -15,17 +15,20 @@ async def send_audio(bot: Bot, event: Event, state: T_State):
     msg_text = decision()
     # msg_date = f"[CQ:at,qq={msg_text[0]}]"
     # msg_pic_text = f"[CQ:at,qq={msg_text[1]}]"
-    if msg_text[3] != None:
-        msg_pic = f"[CQ:image,file={msg_text[3]},type=show,id=40004]"
-        time.sleep(0.5)
-        await today_furry.send(Message(msg_text[1]))
-        time.sleep(0.5)
-        await today_furry.send(Message(msg_text[2]))
-        time.sleep(0.5)
-        await today_furry.send(Message(msg_pic))
-        time.sleep(0.5)
-        print(msg_text)
-    else:
+    try:
+        if msg_text[4] == 1:
+            msg_pic = f"[CQ:image,file={msg_text[3]},type=show,id=40004]"
+            time.sleep(0.5)
+            await today_furry.send(Message(msg_text[1]))
+            time.sleep(0.5)
+            await today_furry.send(Message(msg_text[2]))
+            time.sleep(0.5)
+            await today_furry.send(Message(msg_pic))
+            time.sleep(0.5)
+            print(msg_text)
+        else:
+            await today_furry.send(Message('emmm，今天貌似没有推送唉owo'))
+    except:
         await today_furry.send(Message('emmm，今天貌似没有推送唉owo'))
 
 
